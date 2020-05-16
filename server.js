d8f2c9b5926bdf19c964ee2f3f3e7344b84e5972
pieter1998@protonmail.com <ba8b9d3612615214286ffddbb58bafcc>
2020-05-16 19:12:38 +0200

    added crud functionality

---
+++ server.js
@@ -0,0 +1,106 @@
+const express = require('express')
+const express_graphql = require('express-graphql')
+const {buildSchema} = require('graphql')
+
+//schema
+const schema = buildSchema(`
+ type Query{
+    album(id: Int!): Album
+    albums(genre: String): [Album]
+ }
+ 
+ type Mutation {
+    createAlbum(id: Int!, title: String!, artist: String!, releaseYear: Int!, genre: String!): Album
+    updateAlbumTitle(id: Int!, title: String!): Album
+    deleteAlbum(id: Int!): Album
+ }
+ 
+ type Album{
+    id: Int
+    title: String
+    artist: String
+    releaseYear: Int
+    genre: String
+ }
+`);
+
+let data = [
+    {
+        id: 1,
+        title: 'Killers',
+        artist: 'Iron Maiden',
+        releaseYear: 1981,
+        genre: 'Metal'
+    },
+
+    {
+        id: 2,
+        title: 'Ride the lightning',
+        artist: 'Metallica',
+        releaseYear: 1984,
+        genre: 'Metal'
+    },
+
+    {
+        id: 3,
+        title: 'Moving Pictures',
+        artist: 'Rush',
+        releaseYear: 1981,
+        genre: 'Rock'
+    }
+]
+
+const getAlbum = async (args) => {
+    return data.find(album => {
+        return album.id === args.id;
+    })
+}
+
+const getAlbums = async (args) => {
+    if (args.genre) return data.filter(albums => albums.genre === args.genre)
+}
+
+const updateAlbumTitle = async ({id, title}) => {
+    data.map(album => {
+        if (album.id === id) {
+            album.title = title;
+            return album;
+        }
+    });
+    return data.find(album => {
+        return album.id === id;
+    })
+}
+
+const createAlbum = async ({id, title, artist, releaseYear, genre}) => {
+    data.push({id: id,title: title, artist: artist, releaseYear: releaseYear, genre:  genre})
+    return 0;
+}
+
+const deleteAlbum = async (args) => {
+    data = data.filter(album => album.id !== args.id)
+    return 0;
+}
+
+//resolvers
+const root = {
+    album: getAlbum,
+    albums: getAlbums,
+    createAlbum: createAlbum,
+    updateAlbumTitle: updateAlbumTitle,
+    deleteAlbum: deleteAlbum
+}
+
+//server
+const app = express()
+app.use('/graphql', express_graphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+
+app.use('/', async (req, res ,next) => {
+    res.send({message: 'goto /graphql to write a query'})
+});
+
+app.listen(3000, () => console.log('server running on port 3000 http://localhost:3000/graphql'))
